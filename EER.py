a921c95fa0101c5a44e5074c7220ebeedb1dc18f
Rohan Kuntoji <rohan.kuntoji@gmail.com>
2019-04-20 02:10:37 +0530

    Update EER.py

--- EER.py
+++ EER.py
@@ -4,15 +4,11 @@ import numpy as np
 def evaluateEER(user_scores, imposter_scores):
     labels = [0]*len(user_scores) + [1]*len(imposter_scores)
     fpr, tpr, thresholds = roc_curve(labels, user_scores + imposter_scores)
-#    print 'fpr','tpr','thres',fpr,tpr,thresholds
+
     missrates = 1 - tpr
     farates = fpr
-#    array = np.zeros((123,3))
-#    array[:,0] = missrates
-#    array[:,1] = farates
-#    array[:,2] = thresholds
-#    print array
     dists = missrates - farates
+    
     idx1 = np.argmin(dists[dists >= 0])
     idx2 = np.argmax(dists[dists < 0])
     x = [missrates[idx1], farates[idx1]]
